4de66b5d42533968b4960d8d5489e7b7fbfd9a58
adhammhaydat <adhammohidat123@gmail.com>
2021-08-02 20:21:21 +0300

    solve lab2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import Header from './Component/Header.js'
-import Main from './Component/Main.js'
+import Header from './Component/Header'
+import Main from './Component/Main'
 import Footer from './Component/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <div>
+        <div className='App'>
           <Header />
           <main><Main /></main> 
           <Footer/>
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,29 +1,152 @@
-import React from 'react'
-import HornedBeasts from './HornedBeasts'
+import React from 'react';
+import HornedBeasts from './HornedBeasts';
+import { Container,Col,Row } from 'react-bootstrap';
 
- class Main extends React.Component {
-  
+
+class Main extends React.Component {
+
   render() {
     return (
       <div>
-        <HornedBeasts title="UniWhal" 
-        imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" 
-        description= "A unicorn and a narwhal nuzzling their horns"
-        // onClick={()=>this.raiseClicks()}
-        />
+        {/* <Container fluid="md">
+  <Row>
+    <Col>1 of 1</Col>
+  </Row>
+</Container> */}
+        <Container fluid>
+          <Row>
+            <Col>
+              <HornedBeasts title="UniWhal"
+                imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+                description="A unicorn and a narwhal nuzzling their horns"
 
-        <HornedBeasts title="Rhino Family" 
-        imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" 
-        description= "Mother (or father) rhino with two babies"
-        />
+              /></Col>
 
-        <HornedBeasts title="Unicorn Head" 
-        imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" 
-        description= "Someone wearing a creepy unicorn head mask"
-        />
-        
+            <Col><HornedBeasts title="Rhino Family"
+              imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+              description="Mother (or father) rhino with two babies"
+            /></Col>
+           
+             <Col><HornedBeasts title="Unicorn Head"
+              imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
+              description="Someone wearing a creepy unicorn head mask"
+            /></Col>
+          </Row>
+
+          <Row>
+           
+          <Col>
+              <HornedBeasts title="UniLego"
+                imageUrl="https://images.unsplash.com/photo-1518946222227-364f22132616?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4836a6fca62e7dce9324346bacfde085&auto=format&fit=crop&w=2534&q=80"
+                description="Lego figurine dressed in a unicorn outfit"
+
+              /></Col>
+
+            <Col><HornedBeasts title="Basically a unicorn"
+              imageUrl="https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg"
+              description="A narwhal is basically a unicorn after all, right?"
+            /></Col>
+           
+             <Col><HornedBeasts title="#truth"
+              imageUrl="https://i.pinimg.com/originals/16/cf/2a/16cf2a0b3fd51b9bee08bb6296193b75.jpg"
+              description="The truth behind narwhals"
+            /></Col>
+          </Row>
+          <Row>
+           
+           <Col>
+               <HornedBeasts title="UniLeBaby Rhinogo"
+                 imageUrl="https://secure.img1-ag.wfcdn.com/im/17007094/resize-h800%5Ecompr-r85/3589/35892451/Baby+Rhino+Figurine.jpg"
+                 description="This is actually a figurine but it looks kinda real"
+ 
+               /></Col>
+ 
+             <Col><HornedBeasts title="Cera"
+               imageUrl="https://vignette.wikia.nocookie.net/landbeforetime/images/c/c3/Cera_infobox.png/revision/latest?cb=20180422005003"
+               description="Three horns but still, horns. And who doesn't like The Land Before Time?"
+             /></Col>
+            
+              <Col><HornedBeasts title="Narwhal costume"
+               imageUrl="https://ae01.alicdn.com/kf/HTB18GwSQVXXXXaZaXXXq6xXFXXXh/Animal-Cosplay-Costume-Narwhal-Onesie-Mens-Womens-Cartoon-Whale-Pajamas.jpg"
+               description="A woman wearing a blue narwhal costume"
+             /></Col>
+           </Row>
+           <Row>
+           
+           <Col>
+               <HornedBeasts title="Believe"
+                 imageUrl="https://www.tinselbox.com/wp-content/uploads/2018/03/I-BELIEVE-IN-UNICORNS-FREE-PRINTABLE-WATERCOLOR-7-of-1.jpg"
+                 description="I believe in unicorns, do you?"
+ 
+               /></Col>
+ 
+             <Col><HornedBeasts title="Markhor"
+               imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg/220px-Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg"
+               description="These wild goats eat snakes, then secrete a foam that locals fight over for the antivemon properties -- how cool is that?"
+             /></Col>
+            
+              <Col><HornedBeasts title="Baby markhor"
+               imageUrl="http://www.zooborns.com/.a/6a010535647bf3970b0223c84d5959200c-800wi"
+               description="Even the babies are adorable"
+             /></Col>
+           </Row>
+           <Row>
+           
+           <Col>
+               <HornedBeasts title="Mouflon"
+                 imageUrl="https://images.unsplash.com/photo-1558560063-931ca9822a0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                 description="I believe in unicorns, do you?"
+ 
+               /></Col>
+ 
+             <Col><HornedBeasts title="Addax"
+               imageUrl="https://images.unsplash.com/photo-1556890077-020ec300d5db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
+               description="This guy is basically extinct but survives well in captivity, so they're frequently found in zoos"
+             /></Col>
+            
+              <Col><HornedBeasts title="Baby mouflon"
+               imageUrl="https://cbsnews3.cbsistatic.com/hub/i/r/2013/03/05/5b414225-a645-11e2-a3f0-029118418759/thumbnail/620x350/2d4cf24685b45c22912e64d2004fec8d/Baby_Mouflon_Wild_Sheep.jpg"
+               description="The cuteness that is a baby mouflon asleep"
+             /></Col>
+           </Row>
+           <Row>
+           
+           <Col>
+               <HornedBeasts title="Happy Jackson's Chameleon"
+                 imageUrl="https://images.unsplash.com/photo-1514036783265-fba9577fc473?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                 description="These are really common in Hawaii"
+ 
+               /></Col>
+ 
+             <Col><HornedBeasts title="Serious Jackson's Chameleon"
+               imageUrl="https://imgc.allpostersimages.com/img/print/posters/dlillc-jackson-s-chameleon_a-G-13448768-14258384.jpg"
+               description="This one is very serious."
+             /></Col>
+            
+              <Col><HornedBeasts title="Horned Lizard"
+               imageUrl="https://www.nps.gov/band/learn/nature/images/short-horned-lizard-open-mouth-18.jpg?maxwidth=650&autorotate=false"
+               description="Fave food: ants"
+             /></Col>
+           </Row>
+           <Row>
+           
+           <Col>
+               <HornedBeasts title="Smaug"
+                 imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/290px-Smaug_par_David_Demaret.jpg"
+                 description="Fan illustration of Smaug from 'The Hobbit'"
+ 
+               /></Col>
+ 
+           </Row>
+        </Container>
+
+
+
+
+
+
       </div>
     )
   }
- }
+}
 export default Main
