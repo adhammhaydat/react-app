0f3828d7388454df170c359e2ed9ce808107c10b
adhammhaydat <adhammohidat123@gmail.com>
2021-08-02 16:29:32 +0300

    solvw lab

--- src/Component/HornedBeasts.js
+++ src/Component/HornedBeasts.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
  class HornedBeasts extends React.Component {
+  
+  constructor(){
+    super()
+    this.state={
+      click:0
+    }
+    
+  }
+
+  raiseClicks=()=>{
+   this.setState({
+     click:this.state.click+1
+   })
+  }
+
   render() {
+    
     return (
       <div>
         <h2>{this.props.title} </h2>
-        <img src={this.props.imageUrl} alt={this.props.title} />
+        <img src={this.props.imageUrl} alt={this.props.title} onClick={()=>this.raiseClicks()}/>
         <p>{this.props.description}</p>
+        <p>number of clicks are {this.state.click}</p>
+        
 
       </div>
     )
